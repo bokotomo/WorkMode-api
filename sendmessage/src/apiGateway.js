d5e745261316a096222fcb83b5b3e807c0a0cb71
bokotomo <bokotomo@me.com>
2020-04-13 21:20:48 +0900

    feat

--- sendmessage/src/apiGateway.js
+++ sendmessage/src/apiGateway.js
@@ -1,9 +1,7 @@
 const AWS = require('aws-sdk');
 const apiVersion = '2018-11-29';
 
-module.exports = endpoint => {
-    return new AWS.ApiGatewayManagementApi({
-        apiVersion,
-        endpoint,
-    });
-}
+module.exports = endpoint => new AWS.ApiGatewayManagementApi({
+    apiVersion,
+    endpoint,
+});
