65ff0689991f2b989ea0a467d94a524965fea32f
bokotomo <bokotomo@me.com>
2020-04-18 23:26:15 +0900

    feat: reposiroty user create

--- sendmessage/src/repository/user.js
+++ sendmessage/src/repository/user.js
@@ -3,6 +3,23 @@ const ddbClient = require('../driver/ddb');
 module.exports.create = async (name) => {
     const id = 'iidd'
     const token = 'otokkken'
+
+    const TableName = 'workmode_users'
+    const putParams = {
+        TableName,
+        Item: {
+            id,
+            token,
+            name,
+        }
+    };
+
+    try {
+        await ddbClient.put(putParams).promise();
+    } catch (err) {
+        throw err;
+    }
+
     return {
         id,
         token,
